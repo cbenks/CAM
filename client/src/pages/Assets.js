8606a12633d71366e0617984c64ca59f792e87a2
Cory <cjbenicak@gmail.com>
2022-09-08 10:50:25 -0400

    making layout for a login form

--- client/src/pages/Assets.js
+++ client/src/pages/Assets.js
@@ -1,7 +1,22 @@
 const Assets = ({ authenticated }) => {
+  const authenticatedOptions = (
+    <div>
+      <h2>you are authenticated</h2>
+    </div>
+  )
+  const publicOptions = (
+    <div>
+      <h2>
+        Please login or register to start keeping track of all of your web3
+        assets in one place!
+      </h2>
+    </div>
+  )
+
   return (
     <div>
       <h3>your cryptos and nfts will be listed here</h3>
+      {authenticated ? authenticatedOptions : publicOptions}
     </div>
   )
 }
