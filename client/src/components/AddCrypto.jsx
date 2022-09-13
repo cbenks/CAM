4b1effb6ac042b27f4ea9d697e4a8c0d50e8001c
Cory <cjbenicak@gmail.com>
2022-09-13 10:25:59 -0400

    created forms for entering new crypto and nft assets

--- client/src/components/AddCrypto.jsx
+++ client/src/components/AddCrypto.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 
 const AddCrypto = ({ authenticated, user }) => {
 
-  const [nftForm, setNftForm] = useState({
-
-  })
   const [cryptoForm, setCryptoForm] = useState({
+    name: '',
+    amount: 0
+  })
 
+  const [nftForm, setNftForm] = useState({
+    blockchain: '',
+    name: '',
+    photo: ''
   })
 
   const handleChangeCrypto = (e) => {
@@ -33,19 +37,21 @@ const AddCrypto = ({ authenticated, user }) => {
   )
   let authenticatedOptions = (
     <div>
-      <form>
+      <form onSubmit={handleSubmitCrypto} >
         <label htmlFor="name">Name</label>
-        <input onChange={} name="name" id="name" type="text" />
+        <input onChange={handleChangeCrypto} name="name" id="name" type="text" value={cryptoForm.name} required />
         <label htmlFor="amount">Amount</label>
-        <input onChange={} name="amount" id="amount" type="number" />
-        <button>Add Crypto Asset</button>
+        <input onChange={handleChangeCrypto} name="amount" id="amount" type="number" value={cryptoForm.amount} required/>
+        <button disabled={!cryptoForm.name} >Add Crypto Asset</button>
       </form>
-      <form>
-        <label></label>
-        <input />
-        <label></label>
-        <input />
-        <button>Add Nft Asset</button>
+      <form onSubmit={handleSubmitNft} >
+        <label htmlFor="blockchain">Blockchain</label>
+        <input onChange={handleChangeNft} name="blockchain" id="blockchain" type="text" value={nftForm.blockchain} />
+        <label htmlFor="name">Name</label>
+        <input onChange={handleChangeNft} name="name" id="name" type="text" value={nftForm.name} />
+        <label htmlFor="photo">Photo</label>
+        <input onChange={handleChangeNft} name="photo" id="photo" type="image" value={nftForm.name} />
+        <button disabled={!nftForm.name} >Add Nft Asset</button>
       </form>
     </div>
   )
