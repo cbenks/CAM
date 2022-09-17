f1b4396b944ad3c7854b7cf302b6b74198153b1b
Cory <cjbenicak@gmail.com>
2022-09-17 17:59:57 -0400

    added heroku db

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+// export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'https://cory-cm-db.herokuapp.com/'
 
 const Client = axios.create({ baseURL: BASE_URL })
 
